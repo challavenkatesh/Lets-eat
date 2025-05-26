0038ea85f3ac9b1d03d8030f978831052b4590e0
Challa venkatesh <venkatesh@gmail.com>
2025-05-26 15:05:21 +0530

    Add basename fix and other updates

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import "./index.css";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/Lets-eat">
     <App />
   </BrowserRouter>,
   document.getElementById("root")
